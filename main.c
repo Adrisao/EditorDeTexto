35abd69acf94ff038335ad8bd9a182e5fa54f061
Adrian <sangalletti.adrian.dev@gmail.com>
2026-04-04 10:32:51 -0300

    Adicionado controle basico sobre comportamento do terminal

---
+++ main.c
@@ -0,0 +1,35 @@
+// libs
+#include <stdio.h>
+#include <termios.h>
+
+// configure the terminal
+void enableRawMode(struct termios *original){
+    // save the orignal config
+    tcgetattr(STDIN_FILEMO, original);
+    // create and copy the config
+    struct termios raw;
+    raw = *original;
+    // desable the echo and canon
+    raw.c_lflag &= ~(ECHO | ICANON);
+    // apply
+    tcsetattr(STDIN_FILENO, TCSAFLUSH,  &raw);
+    return;
+}
+
+// Configure back the terminal
+void desableRawMode(struct termios *original){
+    // apply the original config
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, original);
+    return;
+}
+
+// main funcition
+int main(){
+    // start
+    struct termios original;
+    enableRawMode(&original);
+
+    //end
+    desableRawMode(&original);
+    return 0;
+}
